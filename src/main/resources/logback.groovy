925b3f90b3596d4180e36fe701194af5a3a8a56e
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-06-01 13:32:23 +0300

    slf4j and lockback

---
+++ src/main/resources/logback.groovy
@@ -0,0 +1,9 @@
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS} [%thread] %highlight(%-5level) %logger{40}: %msg%n"
+    }
+}
+
+root(INFO, ["STDOUT"])
+
+logger("gesha", INFO)
