4ee5a02906dd1546d02107196f08773c840018fd
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-03-14 15:48:06 +0200

    upgrade to Spring Boot 2

--- src/main/java/gesha/web/config/WebConfiguration.java
+++ src/main/java/gesha/web/config/WebConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
         WebSecurityConfig.class
 })
 @ComponentScan({"gesha.web.interfaces"})
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration implements WebMvcConfigurer {
 
 }
 
